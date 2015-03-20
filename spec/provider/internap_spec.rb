596d23b553feea308c95bfe94335542fc2c5af95
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-03-20 13:24:06 -0700

    add test for power_on

--- spec/provider/internap_spec.rb
+++ spec/provider/internap_spec.rb
@@ -35,7 +35,29 @@ describe KnifeRemote::Provider::Internap do
     })
   end
 
-  it "powers on server" 
+  it "powers on server" do
+    internap = KnifeRemote::Provider::Internap.new("api_key", "api_secret") 
+    allow(Time).to receive(:now).and_return(Time.mktime(2015,3,16,23,1,32,7))
+
+    resp = double(Net::HTTPResponse)
+    allow(resp).to receive(:body)
+
+    params = { 
+      :device_id => "1",
+      :power_action => "on",
+      :method => "voxel.devices.power",
+      :timestamp=>"2015-03-16T23:01:32-07:00" ,
+      :key => "api_key",
+      :api_sig => "4c02c494cae4792416687fe909d72a29"}
+
+    allow(Net::HTTP).to receive(:post_form).with(URI("http://api.voxel.net/version/1.5"), params).and_return(resp)
+
+    internap.voxel_devices_power({
+      :device_id => "1", 
+      :power_action => "on"
+    })
+  end
+
   it "resets server" 
   it "connects to console"
 end
