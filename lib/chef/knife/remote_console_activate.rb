89c2487c10923bbedfeac80633e26acf430d5ad1
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-03-25 15:50:19 -0700

    add internap option to remote console

--- lib/chef/knife/remote_console_activate.rb
+++ lib/chef/knife/remote_console_activate.rb
@@ -1,4 +1,5 @@
 require 'chef/knife/remote_base'
+require 'chef/knife'
 
 class Chef
   class Knife
@@ -8,7 +9,18 @@ class Chef
 
       banner "knife remote console activate NODE"
 
+      option :internap,
+        :short => "-i",
+        :long => "--internap",
+        :boolean => true,
+        :default => false
+
       def run
+        if config[:internap] && Kernel.system("which sshpass") == false
+          ui.fatal "sshpass required but not found"  
+          exit 2
+        end 
+
         server.console
       end
     end
