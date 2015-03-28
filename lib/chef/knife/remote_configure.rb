1a908ecb979f2158a80c3ee857844515bd6418d1
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-03-27 17:10:25 -0700

    configure local

---
+++ lib/chef/knife/remote_configure.rb
@@ -0,0 +1,22 @@
+require 'chef/knife/remote_base'
+
+class Chef
+  class Knife
+    class RemoteConfigure < Knife
+    
+      include Knife::RemoteBase    
+
+      banner "knife remote configure"
+
+      def run
+        puts %Q{ 
+Place the following in your knife.rb:
+
+knife[:ipmi_user] = "username" ipmi username (required)
+knife[:ipmi_pass] = "password" ipmi password (required)
+knife[:ipmi_ohai_ip] = "ipmi.ip.attribute" ohai attribute with the value of a server's ipmi ip (optional) 
+        }
+      end
+    end
+  end
+end
