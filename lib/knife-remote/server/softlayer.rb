f1f6024adfd59547fa45fe5ee372648fbd0de8e5
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-04-01 23:23:50 -0700

    softlayer server status impl

---
+++ lib/knife-remote/server/softlayer.rb
@@ -0,0 +1,33 @@
+require "softlayer_api"
+
+module KnifeRemote
+  module Server
+    class Softlayer
+      def initialize(hostname)
+        @hostname = hostname
+      end
+
+      def on? 
+        hostname = /^([a-z0-9-]*)(?=\..*)/.match(@hostname)[0]
+        filter = SoftLayer::ObjectFilter.new do |filter|
+          filter.accept("hardware.hostname").when_it is(hostname)
+        end
+        api= softlayer
+        server = api.service_named("Account").object_filter(filter).object_mask("mask[id]").getHardware
+        api.service_named("Hardware_Server").object_with_id(server[0]["id"]).getServerPowerState == "on"
+      end
+
+      def off? 
+        !on?
+      end
+
+      private
+      def softlayer
+        SoftLayer::Client.new({
+          :api_key => Chef::Config[:knife][:softlayer_api_key],
+          :username => Chef::Config[:knife][:softlayer_user]
+          })
+      end
+    end
+  end
+end
