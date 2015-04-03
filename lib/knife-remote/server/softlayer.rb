5e70993149ef57c065c7e6a81e56b7302361c385
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-04-03 11:23:41 -0700

    power on implementation

--- lib/knife-remote/server/softlayer.rb
+++ lib/knife-remote/server/softlayer.rb
@@ -31,6 +31,16 @@ module KnifeRemote
         api.service_named("Hardware_Server").object_with_id(server[0]["id"]).powerOff
       end
 
+      def on 
+        hostname = /^([a-z0-9-]*)(?=\..*)/.match(@hostname)[0]
+        filter = SoftLayer::ObjectFilter.new do |filter|
+          filter.accept("hardware.hostname").when_it is(hostname)
+        end
+        api= softlayer
+        server = api.service_named("Account").object_filter(filter).object_mask("mask[id]").getHardware
+        api.service_named("Hardware_Server").object_with_id(server[0]["id"]).powerOn
+      end
+
       private
       def softlayer
         SoftLayer::Client.new({
