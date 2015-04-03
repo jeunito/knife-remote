6656047e4e64f0dc4c93c2e4ff573a4822b1560a
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-04-03 16:32:25 -0700

    reset feature implementation

--- lib/knife-remote/server/softlayer.rb
+++ lib/knife-remote/server/softlayer.rb
@@ -51,6 +51,19 @@ module KnifeRemote
         api.service_named("Hardware_Server").object_with_id(server[0]["id"]).rebootHard
       end
 
+      def console
+        hostname = /^([a-z0-9-]*)(?=\..*)/.match(@hostname)[0]
+        filter = SoftLayer::ObjectFilter.new do |filter|
+          filter.accept("hardware.hostname").when_it is(hostname)
+        end
+        api= softlayer
+        server = api.service_named("Account").object_filter(filter).object_mask("mask[id]").getHardware
+        ipmi_account = api.service_named("Hardware_Server").object_with_id(server[0]["id"]).getRemoteManagementAccounts
+        ipmi_server = api.service_named("Hardware_Server").object_with_id(server[0]["id"]).getRemoteManagementComponent
+
+        exec("ipmitool -U #{ipmi_account["username"]} -P #{ipmi_account["password"]} -H #{ipmi_server["ipmiIpAddress"]} -I lanplus sol activate")
+      end
+
       private
       def softlayer
         SoftLayer::Client.new({
