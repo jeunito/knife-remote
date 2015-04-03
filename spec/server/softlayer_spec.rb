bf602ca9dd68ea436813d197378b5d2e93aefb8e
jose.asuncion@gmail.com <jose.asuncion@gmail.com>
2015-04-03 11:32:50 -0700

    reboot server impl for softlayer

--- spec/server/softlayer_spec.rb
+++ spec/server/softlayer_spec.rb
@@ -88,4 +88,32 @@ describe "softlayer server" do
       softlayer_server = KnifeRemote::Server::Softlayer.new("server.example.com")
       softlayer_server.on 
   end
+
+  it "reboots server" do
+      accounts_service = double()
+      accounts_filter = double()
+      accounts_mask = double()
+      server_service = double()
+      server_object = double()
+      api = double()
+
+      allow(Chef::Config).to receive(:[]).with(:knife).and_return({:softlayer_api_key => "api_key", :softlayer_user => "user" })
+
+      expect(SoftLayer::Client).to receive(:new).with({:username => "user", :api_key => "api_key" }).and_return(api)
+
+      expect(api).to receive(:service_named).with("Account").and_return(accounts_service)
+
+      expect(accounts_service).to receive(:object_filter).and_return(accounts_filter)
+      expect(accounts_filter).to receive(:object_mask).with("mask[id]").and_return(accounts_mask)
+      expect(accounts_mask).to receive(:getHardware).and_return([{"id" => 1, "fullyQualifiedDomainName" => "server.example.com"}])
+
+      expect(api).to receive(:service_named).with("Hardware_Server").and_return(server_service)
+  
+      expect(server_service).to receive(:object_with_id).with(1).and_return(server_object)
+
+      expect(server_object).to receive(:rebootHard)
+
+      softlayer_server = KnifeRemote::Server::Softlayer.new("server.example.com")
+      softlayer_server.reset 
+  end
 end
